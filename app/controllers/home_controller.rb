df6e56ff24b66276b9725f851f56150bb276c335
Josh Wykell <jwykell@gmail.com>
2015-06-24 18:57:54 -0400

    changed active record string query to single quote

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   
   def index
-    @recent_lists = List.order("created_at DESC").limit(5)
+    @recent_lists = List.order('created_at DESC').limit(5)
   end
 
   # def recent_list_id
